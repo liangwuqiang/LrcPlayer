6a5fda719ce35ed49dea71a98147d5170985af93
liangwuqiang <l_wuqiang@126.com>
2018-04-09 15:27:11 +0800

    文件管理器提取文件

--- app/src/main/java/com/slwy/lwq/lrcplayer/MainActivity.java
+++ app/src/main/java/com/slwy/lwq/lrcplayer/MainActivity.java
@@ -1,19 +1,40 @@
 package com.slwy.lwq.lrcplayer;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+//import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Uri uri;      //存储影音文件的 Uri
+    TextView txtContent;  //引用到画面中的组件
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //设置屏幕不随手机旋转、以及画面直向显示
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_NOSENSOR);//设置屏幕不随手机旋转
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);//设置屏幕直向显示
+
+        uri = Uri.parse("android.resource://" + //默认会播放程序内的音乐文件
+                getPackageName() + "/" + R.raw.welcome);
+
+        txtContent = findViewById(R.id.txtContent); //引用到第1个文字组件
+        txtContent.setText(uri.getLastPathSegment ());//显示 Uri
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
     }
 
@@ -36,7 +57,37 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
 
+        if (id == R.id.action_open) {
+            onPick();
+//            return true;
+        }
+
         return super.onOptionsItemSelected(item);
     }
 
+    public void onPick() {
+        Intent it = new Intent(Intent.ACTION_GET_CONTENT);    //创建动作为 "选取" 的 Intent
+        it.setType("audio/*");     //要选取所有音乐类型
+        startActivityForResult(it, 100);
+    }
+    protected void onActivityResult (int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == Activity.RESULT_OK) {
+            uri = convertUri(data.getData());  //获取选取文件的 Uri 并进行 Uri 格式转换
+            txtContent.setText(uri.getLastPathSegment ());  //显示文件名 (Uri 最后的节点文字)
+        }
+    }
+    //将 "content://" 类型的 Uri 转换为 "file://" 的 Uri
+    Uri convertUri(Uri uri) {
+        if(uri.toString().substring(0, 7).equals("content")) {  //如果是以 "content" 开头
+            String[] colName = { MediaStore.MediaColumns.DATA };    //声明要查询的字段
+            Cursor cursor = getContentResolver().query(uri, colName,  //以 uri 进行查询
+                    null, null, null);
+            cursor.moveToFirst();      //移到查询结果的第一个记录
+            uri = Uri.parse("file://" + cursor.getString(0)); //将路径转为 Uri
+            cursor.close();     //关闭查询结果
+        }
+        return uri;   //返回 Uri 对象
+    }
 }
