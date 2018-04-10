77da28e3502e0e34be473c4ff89987b29731efd0
liangwuqiang <l_wuqiang@126.com>
2018-04-10 10:45:22 +0800

    可以播放基本的音乐

--- app/src/main/java/com/slwy/lwq/lrcplayer/MainActivity.java
+++ app/src/main/java/com/slwy/lwq/lrcplayer/MainActivity.java
@@ -4,20 +4,29 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.database.Cursor;
+import android.media.MediaPlayer;
 import android.net.Uri;
+import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-//import android.view.View;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements
+        MediaPlayer.OnPreparedListener,   //实现 MediaPlayer 的 3 个的事件监听接口
+        MediaPlayer.OnErrorListener,
+        MediaPlayer.OnCompletionListener {
 
-    Uri uri;      //存储影音文件的 Uri
-    TextView txtContent;  //引用到画面中的组件
+    Uri uri;      //声音文件的 Uri
+    TextView txtContent;
+    Button btnPlay;
+    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,67 +36,152 @@ public class MainActivity extends AppCompatActivity {
         //设置屏幕不随手机旋转、以及画面直向显示
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_NOSENSOR);//设置屏幕不随手机旋转
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);//设置屏幕直向显示
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);//设置屏幕不进入休眠
 
         uri = Uri.parse("android.resource://" + //默认会播放程序内的音乐文件
                 getPackageName() + "/" + R.raw.welcome);
 
-        txtContent = findViewById(R.id.txtContent); //引用到第1个文字组件
-        txtContent.setText(uri.getLastPathSegment ());//显示 Uri
+        txtContent = findViewById(R.id.txtContent);
+        btnPlay = findViewById(R.id.btnPlay);
+
+        txtContent.setText(R.string.welcome);
+        btnPlay.setText(R.string.pause);
+
+        mediaPlayer = new MediaPlayer();           //创建 MediaPlayer 对象
+        mediaPlayer.setOnPreparedListener(this);   //设置 3 个事件监听器
+        mediaPlayer.setOnErrorListener(this);
+        mediaPlayer.setOnCompletionListener(this);
+
+        prepareMusic();
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+    protected void onPause() {
+        super.onPause();
+
+        if (mediaPlayer.isPlaying()) {  //如果正在播, 就暂停
+            btnPlay.setText("继续");
+            mediaPlayer.pause();  //暂停播放
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        mediaPlayer.release();  //释放 MediaPlayer 对象
+        super.onDestroy();
+    }
+
+    //实现接口的三个函数=====================================================================
+    @Override
+    public void onPrepared(MediaPlayer mp) {
+        btnPlay.setText(R.string.play);  //按钮改"播放"
+    }
+
+    @Override
+    public void onCompletion(MediaPlayer mp) {
+        mediaPlayer.seekTo(0);
+        btnPlay.setText(R.string.play);
+    }
+
+    @Override
+    public boolean onError(MediaPlayer mp, int what, int extra) {
+        Toast.makeText(this, "出错停止", Toast.LENGTH_SHORT).show();
+        return true;
+    }
+
+    //处理菜单点击事件================================================================
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {  //显示菜单
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+    public boolean onOptionsItemSelected(MenuItem item) {  //菜单选择，相应的点击事件
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
-
         if (id == R.id.action_open) {
             onPick();
-//            return true;
+            return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
-    public void onPick() {
-        Intent it = new Intent(Intent.ACTION_GET_CONTENT);    //创建动作为 "选取" 的 Intent
-        it.setType("audio/*");     //要选取所有音乐类型
+    public void onPick() {  //菜单点击事件
+        Intent it = new Intent(Intent.ACTION_GET_CONTENT);
+        it.setType("audio/*");     //音乐类型
         startActivityForResult(it, 100);
     }
-    protected void onActivityResult (int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
 
+    protected void onActivityResult (int requestCode, int resultCode, Intent data) {  //回调方法
+        super.onActivityResult(requestCode, resultCode, data);
         if(resultCode == Activity.RESULT_OK) {
-            uri = convertUri(data.getData());  //获取选取文件的 Uri 并进行 Uri 格式转换
-            txtContent.setText(uri.getLastPathSegment ());  //显示文件名 (Uri 最后的节点文字)
+            uri = convertUri(data.getData());
+            txtContent.setText(uri.getLastPathSegment ());
+            prepareMusic();
         }
     }
-    //将 "content://" 类型的 Uri 转换为 "file://" 的 Uri
-    Uri convertUri(Uri uri) {
-        if(uri.toString().substring(0, 7).equals("content")) {  //如果是以 "content" 开头
+
+    Uri convertUri(Uri uri) {  //将"content://"类型的Uri转换为"file://"的Uri
+        if(uri.toString().substring(0, 7).equals("content")) {  //如果是以"content"开头
             String[] colName = { MediaStore.MediaColumns.DATA };    //声明要查询的字段
-            Cursor cursor = getContentResolver().query(uri, colName,  //以 uri 进行查询
+            Cursor cursor = getContentResolver().query(uri, colName,  //以uri进行查询
                     null, null, null);
-            cursor.moveToFirst();      //移到查询结果的第一个记录
-            uri = Uri.parse("file://" + cursor.getString(0)); //将路径转为 Uri
-            cursor.close();     //关闭查询结果
+            if (cursor != null){
+                cursor.moveToFirst();      //移到查询结果的第一个记录
+                uri = Uri.parse("file://" + cursor.getString(0)); //将路径转为 Uri
+                cursor.close();
+            }
         }
-        return uri;   //返回 Uri 对象
+        return uri;
     }
+
+
+
+    //播放初始化, 在主界面调用 和 选定文件的回调方法中使用======================================
+    void prepareMusic() {
+        btnPlay.setText(R.string.play);   //按钮改"播放"
+        try {
+            mediaPlayer.reset();  //复位，开始播放前都这样处理
+            mediaPlayer.setDataSource(this, uri);
+            mediaPlayer.prepareAsync();
+        } catch (Exception e) {
+            Toast.makeText(this, "错误:" + e.toString(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    //界面按钮点击事件的方法==============================================================
+    public void onMpPlay(View v) {  //对应于播放按钮的点击事件，ok
+        if (mediaPlayer.isPlaying()) {  //正在播放时，暂停，按钮改"播放"
+            mediaPlayer.pause();
+            btnPlay.setText(R.string.play);
+        }
+        else {
+            mediaPlayer.start();  //不播放时，开始播放，按钮改"暂停"
+            btnPlay.setText(R.string.pause);
+        }
+    }
+
+    public void onMpBackward(View v) {
+        if(!mediaPlayer.isPlaying()) return;
+        int pos = mediaPlayer.getCurrentPosition();
+        pos -= 10000;  //倒退 10 秒 (10000ms)
+        if(pos <0) pos = 0;
+        mediaPlayer.seekTo(pos);
+    }
+
+    public void onMpForward(View v) {
+        if(!mediaPlayer.isPlaying()) return;
+        int len = mediaPlayer.getDuration();
+        int pos = mediaPlayer.getCurrentPosition();
+        pos += 10000;  //前进 10 秒 (10000ms)
+        if(pos > len) pos = len;
+        mediaPlayer.seekTo(pos);
+    }
+
 }
--- app/src/main/java/com/slwy/lwq/lrcplayer/Temp.java
+++
@@ -1,8 +0,0 @@
-package com.slwy.lwq.lrcplayer;
-
-/**
- * Created by l_wuq on 2018/4/9.
- */
-
-public class Temp {
-}
